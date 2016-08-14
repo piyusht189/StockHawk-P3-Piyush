b546ec4dd179d0a04ef4d4900af86a747bb411f3
Super-Nova <piyusht189@gmail.com>
2016-08-14 16:10:14 +0530

    Initial commit

---
+++ app/src/main/java/com/piyush/android/stockhawk/touch_helper/ItemTouchHelperAdapter.java
@@ -0,0 +1,11 @@
+package com.piyush.android.stockhawk.touch_helper;
+
+/**
+ * Created by piyush on 10/6/15.
+ * credit to Paul Burke (ipaulpro)
+ * Interface to enable swipe to delete
+ */
+public interface ItemTouchHelperAdapter {
+
+  void onItemDismiss(int position);
+}
---
+++ app/src/main/java/com/piyush/android/stockhawk/touch_helper/ItemTouchHelperViewHolder.java
@@ -0,0 +1,12 @@
+package com.piyush.android.stockhawk.touch_helper;
+
+/**
+ * Created by piyush on 10/6/15.
+ * credit to Paul Burke (ipaulpro)
+ * Interface for enabling swiping to delete
+ */
+public interface ItemTouchHelperViewHolder {
+  void onItemSelected();
+
+  void onItemClear();
+}
